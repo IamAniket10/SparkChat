55e73c9daf0103618730785bdf1f609c8a68dbfd
AniketKadam29 <89138365+IamAniket10@users.noreply.github.com>
2024-06-24 20:57:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ app.use(cors({
 app.use(express.json())
 app.use(cookiesParser())
 
+app.options('*', cors())
+
+
+app.use(function(req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, client-security-token')
+  next()
+})
+
 const PORT = process.env.PORT || 8080
 
 app.get('/',(request,response)=>{
