805588a4341191dfe0aab61bf8ff36757ed55432
AniketKadam29 <89138365+IamAniket10@users.noreply.github.com>
2024-06-25 10:37:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,12 +17,9 @@ app.use(cookiesParser())
 app.options('*', cors())
 
 
-// app.use(function(req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, client-security-token')
-//   next()
-// })
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
 
 const PORT = process.env.PORT || 8080
 
